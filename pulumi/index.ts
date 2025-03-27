2220ddde10ab548870a91c8a30affb49c48053e9
Engin Diri <engin.diri@ediri.de>
2025-03-27 09:29:32 +0100

    feat: add simple Pulumi

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,43 +1,68 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
 import * as eks from "@pulumi/eks";
+import * as k8s from "@pulumi/kubernetes";
 
-// Grab some values from the Pulumi configuration (or use default values)
-const config = new pulumi.Config();
-const minClusterSize = config.getNumber("minClusterSize") || 3;
-const maxClusterSize = config.getNumber("maxClusterSize") || 6;
-const desiredClusterSize = config.getNumber("desiredClusterSize") || 3;
-const eksNodeInstanceType = config.get("eksNodeInstanceType") || "t3.medium";
-const vpcNetworkCidr = config.get("vpcNetworkCidr") || "10.0.0.0/16";
-
-// Create a new VPC
-const eksVpc = new awsx.ec2.Vpc("eks-vpc", {
+// Create a VPC for our EKS cluster
+const vpc = new aws.ec2.Vpc("eks-vpc", {
+    cidrBlock: "10.0.0.0/16",
     enableDnsHostnames: true,
-    cidrBlock: vpcNetworkCidr,
+    enableDnsSupport: true,
+    tags: { Name: "eks-vpc" },
 });
 
-// Create the EKS cluster
+// Create subnets for our VPC
+const subnet = new aws.ec2.Subnet("eks-subnet", {
+    vpcId: vpc.id,
+    cidrBlock: "10.0.1.0/24",
+    availabilityZone: "us-west-2a",
+    tags: { Name: "eks-subnet" },
+});
+
+// Create a security group for our EKS cluster
+const securityGroup = new aws.ec2.SecurityGroup("eks-security-group", {
+    vpcId: vpc.id,
+    description: "Allow all HTTP(s) traffic",
+    ingress: [
+        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
+    ],
+    egress: [
+        { protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] },
+    ],
+    tags: { Name: "eks-security-group" },
+});
+
+// Create an EKS cluster
 const eksCluster = new eks.Cluster("eks-cluster", {
-    // Put the cluster in the new VPC created earlier
-    vpcId: eksVpc.vpcId,
-    // Use the "API" authentication mode to support access entries
-    authenticationMode: eks.AuthenticationMode.Api,
-    // Public subnets will be used for load balancers
-    publicSubnetIds: eksVpc.publicSubnetIds,
-    // Private subnets will be used for cluster nodes
-    privateSubnetIds: eksVpc.privateSubnetIds,
-    // Change configuration values to change any of the following settings
-    instanceType: eksNodeInstanceType,
-    desiredCapacity: desiredClusterSize,
-    minSize: minClusterSize,
-    maxSize: maxClusterSize,
-    // Do not give the worker nodes public IP addresses
-    nodeAssociatePublicIpAddress: false,
-    // Change these values for a private cluster (VPN access required)
-    endpointPrivateAccess: false,
-    endpointPublicAccess: true,
+    vpcId: vpc.id,
+    subnetIds: [subnet.id],
+    instanceType: "t2.medium",
+    desiredCapacity: 2,
+    minSize: 1,
+    maxSize: 3,
+    storageClasses: "gp2",
+    deployDashboard: false,
+    skipDefaultNodeGroup: true,
+    tags: { Name: "eks-cluster" },
 });
 
-// Export some values for use elsewhere
-export const kubeconfig = eksCluster.kubeconfig;
-export const vpcId = eksVpc.vpcId;
+// Export the EKS cluster name
+export const eksClusterName = eksCluster.eksCluster.name;
+
+// Create a Kubernetes provider instance
+const k8sProvider = new k8s.Provider("k8s-provider", {
+    kubeconfig: eksCluster.kubeconfig,
+});
+
+// Deploy the CoreDNS Helm chart
+const corednsRelease = new k8s.helm.v3.Release("coredns", {
+    chart: "coredns",
+    repositoryOpts: {
+        repo: "https://charts.helm.sh/stable",
+    },
+    values: {}, // Add any custom values here
+}, { provider: k8sProvider });
+
+// Export the CoreDNS release name
+export const corednsReleaseName = corednsRelease.name;
